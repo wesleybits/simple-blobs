7f2f0ddde605413f61ac9bda38f7aee0ee8718fd
Wesley Bitomski <wesley.bitomski@gmail.com>
2023-06-01 16:12:08 -0400

    Add golang (#2)
    * update README
    
    * adds Golang implmentation
    
    * move scala implementation
    
    * drop these

--- src/main/scala/AkkaEnv.scala
+++
@@ -1,8 +0,0 @@
-package simpleglobs
-import akka.actor.typed._
-import scala.concurrent.ExecutionContext
-
-trait AkkaEnv {
-  implicit val system: ActorSystem[_]
-  implicit val executionContext: ExecutionContext
-}
